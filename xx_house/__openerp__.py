799185c37376af730c386288cb66c2ba2228c9b4
Glenn Smeulders <Glenn@MacBook-Pro-van-Glenn.local>
2016-04-25 12:10:42 +0200

    Update

--- xx_house/__openerp__.py
+++ xx_house/__openerp__.py
@@ -6,12 +6,9 @@
 Dit is de module waarin huizen kunnen beheerd worden
 """,
     'version': '1.0',
-<<<<<<< HEAD
     'depends': ['base', 'product'],
     'data': ['views/house.xml', 'views/attribute.xml'],
-=======
     'depends': ['base', 'product', 'xx_partner'],
     'data': ['views/house.xml'],
->>>>>>> f7d7a5c69215bb7781df4ab9e0ab93f6612b7447
     'installable': True,
 }
--- xx_partner/models/partner.py
+++ xx_partner/models/partner.py
@@ -1,12 +1,11 @@
-
-
 from openerp import models, fields, api, exceptions
+
 
 class User(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
-    name = fields.Char(compute = "_createName", store=True, default="")
+    name = fields.Char(compute="_createName", store=True, default="")
     xx_lastName = fields.Char(string="Achternaam", required=True)
     xx_firstName = fields.Char(string="Voornaam", required=True)
     xx_street = fields.Char(string="Straat")
@@ -15,20 +14,20 @@ class User(models.Model):
     xx_areaCode = fields.Char(string="Postcode")
     xx_telephone = fields.Char(string="Telefoonnummer")
     xx_cellphone = fields.Char(string="GSM-nummer")
-    #wachtwoord
+    # wachtwoord
     xx_email = fields.Char(string="E-mailadres", required=True)
     xx_supplier = fields.Boolean(string="Is verkoper")
 
-    xx_buyTransaction_ids = fields.One2many('xx.transaction','xx_buyer_id', string='Houses bought')
-    #xx_housesOnSale_ids = fields.One2many('product.template','xx_seller_id', string='Houses on sale')
+    xx_buyTransaction_ids = fields.One2many('xx.transaction', 'xx_buyer_id', string='Houses bought')
+
+    # xx_housesOnSale_ids = fields.One2many('product.template','xx_seller_id', string='Houses on sale')
 
     @api.depends('xx_firstName', 'xx_lastName')
     def _createName(self):
         self.name = self.xx_lastName + ' ' + self.xx_firstName
 
-    @api.constrains('telephone','cellphone')
+    @api.constrains('telephone', 'cellphone')
     def _check_telephone_or_cellphone_empty(self):
         for r in self:
             if not r.telephone and not r.cellphone:
                 raise exceptions.ValidationError("Telefoon of gsm nummer moet ingevuld zijn")
-
