89bff2fb4dc4340cfce9a7e4c4aa54201016120d
Bonar <jonas.elst@student.ap.be>
2016-05-29 19:26:29 +0200

    Merge2

---
+++ theme_bootswatch_native/__openerp__.py
@@ -0,0 +1,30 @@
+{
+    'author': "Immomakers",
+    'website': "https://www.ViaSofie.be",
+    'name': 'ViaSoTheme',
+    'summary': 'Standaard thema voor de website',
+    'description': 'Mooi',
+    'category': 'Theme',
+    'version': '1.1',
+
+    'depends': ['website','sale','xx_realEstate'],
+    'data': ['views/bootswatch-native.xml',
+             'views/welkom.xml',
+             'views/footer.xml',
+             'views/referenties.xml',
+             'views/advies.xml',
+             'views/woningen.xml',
+             'views/contact.xml',
+             'views/woningview.xml',
+             'views/mainlayout.xml',
+             'views/submenu.xml',
+             'views/showlogo.xml',
+             'views/compdesc.xml'
+
+             
+             
+
+
+            ],
+    'application': False,
+}
