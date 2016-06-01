1d8a8c2e68780eaa58df61028cfca3864830b6ab
Glenn Smeulders <Glenn@MacBook-Pro-van-Glenn.local>
2016-06-01 09:15:31 +0200

    Mail template fix

--- xx_realEstate/models/partner.py
+++ xx_realEstate/models/partner.py
@@ -1,5 +1,6 @@
 import re
-
+from urlparse import urljoin
+import werkzeug
 from openerp import models, fields, api, exceptions
 
 
@@ -15,7 +16,6 @@ class User(models.Model):
     xx_telephone = fields.Char(string="Telefoonnummer")
     xx_cellphone = fields.Char(string="GSM-nummer")
     xx_has_login = fields.Boolean('Heeft login')
-    # wachtwoord
 
     email = fields.Char(string="E-mailadres", required=True)
     xx_type = fields.Selection(
@@ -23,7 +23,6 @@ class User(models.Model):
         string='Type', required=True)
 
     xx_buyTransaction_ids = fields.One2many('xx.transaction', 'xx_buyer_id', string='Huizen gekocht')
-
     xx_housesOnSale_ids = fields.One2many('product.template', 'xx_seller_id', string='Huizen te koop')
     xx_visitor_ids = fields.One2many('xx.house.visitors', 'name', string="Huizen bezichtigd")
 
@@ -67,7 +66,8 @@ class User(models.Model):
 
     @api.multi
     def action_apply(self):
-        self.write({'user_id': self._create_user()})
+        user_id = self._create_user()
+        self.write({'user_id': user_id})
         self._send_email()
 
     @api.multi
@@ -78,7 +78,7 @@ class User(models.Model):
             'partner_id': self.id,
             'groups_id': [(6, 0, [])]
         }
-
+        self._context.update = dict(self._context or {}, noshortcut=True, no_reset_password=True)
         user = self.env['res.users'].create(values)
         return user.id
 
@@ -88,13 +88,57 @@ class User(models.Model):
         user = self.user_id
         context = dict({}, lang=user.lang)
         ctx_portal_url = dict(context, signup_force_type_in_url='')
-        portal_url = self._get_signup_url_for_action([user.partner_id.id], context=ctx_portal_url)[
+        portal_url = self._get_signup_url_for_action([user.partner_id], context=ctx_portal_url)[
             user.partner_id.id]
         self.signup_prepare([user.partner_id.id], context=context)
-
         context.update({'dbname': self._cr.dbname, 'portal_url': portal_url})
         template_id = self.env['ir.model.data'].xmlid_to_res_id('portal.mail_template_data_portal_welcome')
         for partner in self:
             if template_id:
-                self.env['mail.template'].browse(template_id).send_mail(partner.id, force_send=True)
-        return True
+                self.pool['mail.template'].send_mail(self._cr, self._uid, template_id, partner.id, force_send=True,
+                                                     context=context)
+
+    @api.v7
+    def _get_signup_url_for_action(self, cr, uid, ids, action=None, view_type=None, menu_id=None, res_id=None,
+                                   model=None, context=None):
+        if context is None:
+            context = {}
+        res = dict.fromkeys(ids, False)
+        base_url = self.pool.get('ir.config_parameter').get_param(cr, uid, 'web.base.url')
+        for partner in self.browse(cr, uid, ids, context):
+            if context.get('signup_valid') and not partner.user_ids:
+                self.signup_prepare(cr, uid, [partner.id], context=context)
+            route = 'login'
+            query = dict(db=cr.dbname)
+            signup_type = context.get('signup_force_type_in_url', partner.signup_type or '')
+            if signup_type:
+                route = 'reset_password' if signup_type == 'reset' else signup_type
+            if partner.signup_token and signup_type:
+                query['token'] = partner.signup_token
+            elif partner.user_ids:
+                query['login'] = partner.user_ids[0].login
+            else:
+                continue
+
+            fragment = dict()
+            base = '/web#'
+            if action == '/mail/view':
+                base = '/mail/view?'
+            elif action:
+                fragment['action'] = action
+            if view_type:
+                fragment['view_type'] = view_type
+            if menu_id:
+                fragment['menu_id'] = menu_id
+            if model:
+                fragment['model'] = model
+            if res_id:
+                fragment['res_id'] = res_id
+            if fragment:
+                query['redirect'] = base + werkzeug.url_encode(fragment)
+
+            url = urljoin(base_url, "/web/%s?%s" % (route, werkzeug.url_encode(query)))
+            if '[1]' in url:
+                url = url.replace('[1]', 'signup')
+            res[partner.id] = url
+        return res
