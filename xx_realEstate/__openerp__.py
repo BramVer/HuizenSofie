b577beae066da70f0c187b236e4272127c711cde
Glenn Smeulders <Glenn@MacBook-Pro-van-Glenn.local>
2016-06-10 15:54:49 +0200

    access rights update

--- xx_realEstate/__openerp__.py
+++ xx_realEstate/__openerp__.py
@@ -6,7 +6,7 @@
 Dit is de module voor viaSofie
 """,
     'version': '1.0',
-    'depends': ['base', 'product', 'calendar'],
+    'depends': ['base', 'product', 'calendar', 'website_sale'],
     'data': ['views/partner.xml', 'views/transaction.xml', 'views/attribute.xml', 'views/house.xml', 'views/city.xml',
              'views/house_type.xml', 'views/document.xml', 'views/house_sequence.xml', 'views/visitor.xml',
              'views/status.xml', 'views/ebook.xml', 'security/ir.model.access.csv'],
